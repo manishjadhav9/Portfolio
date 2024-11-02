c45254cb8c94e2842b7424425cb30445afc3ea28
Manish Jadhav <manishsj289@gmail.com>
2024-11-02 18:02:37 +0530

    footer info changed

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -110,7 +110,7 @@ const Footer = () => {
             <InstagramIcon />
           </SocialMediaIcon>
         </SocialMediaIcons>
-        <Copyright>&copy; 2024 Rishav Chanda. All rights reserved.</Copyright>
+        <Copyright>&copy; 2024 Manish Jadhav. All rights reserved.</Copyright>
       </FooterWrapper>
     </FooterContainer>
   );
